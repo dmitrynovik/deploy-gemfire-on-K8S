3570cd01a851ded43f31d7e7b551cc03230e2f89
dmitrtynovik <cryinstone@gmail.com>
2022-08-16 08:24:22 +1000

    feat: replicas & servers

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ cluster_name="gemfire-cluster"
 create_role_binding=1
 install_helm=1
 install_cert_manager=1
+servers=1
+replicas=1
 
 while [ $# -gt 0 ]; do
 
@@ -83,6 +85,8 @@ echo "CREATE $clustername CLUSTER"
 ytt -f gemfire-crd.yml \
      --data-value-yaml cluster_name=$cluster_name \
      --data-value-yaml image="imageregistry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-k8s:$gemfire_version" \
+     --data-value-yaml servers=$servers \
+     --data-value-yaml replicas=$replicas \
      | $kubectl --namespace=$namespace apply -f-
 
 $kubectl -n $namespace get GemFireClusters
