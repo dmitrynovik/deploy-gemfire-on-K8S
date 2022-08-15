7969dded2735a51885e32a000c207b60f9d10fd1
dmitrynovik <cryinstone@gmail.com>
2022-08-14 22:33:14 -0700

    fix: WIP

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@ set -eo pipefail
 serviceaccount=rabbitmq
 namespace="tanzu-gemfire"
 kubectl=kubectl
-registry="registry.pivotal.io"
-version="9.15.1"
+registry="registry.tanzu.vmware.com"
+operator_version="2.0.0"
+gemfire_version="9.15.0"
 cluster_name="gemfire-cluster"
+create_role_binding=1
+install_helm=1
 
 while [ $# -gt 0 ]; do
 
@@ -39,12 +42,41 @@ echo "CREATE NAMESPACE $namespace if it does not exist..."
 $kubectl create namespace $namespace --dry-run=client -o yaml | $kubectl apply -f-
 
 echo "CREATE DOCKER REGISTRY SECRET"
-$kubectl create secret docker-registry image-pull-secret --namespace=$namespace --docker-server=$registry --docker-username="$vmwareuser" --docker-password="$vmwarepassword" --dry-run=client -o yaml | $kubectl apply -f-
+$kubectl create secret docker-registry image-pull-secret --namespace=$namespace --docker-server=$registry \
+     --docker-username="$vmwareuser" --docker-password="$vmwarepassword" --dry-run=client -o yaml \
+     | $kubectl apply -f-
+
+if [ $create_role_binding -eq 1 ]
+then
+     echo "CREATE ROLE BINDING"
+     kubectl create rolebinding psp-gemfire --namespace=$namespace \
+          --clusterrole=psp:vmware-system-privileged --serviceaccount=$namespace:default \
+          --dry-run=client -o yaml | $kubectl apply -f-
+fi
+
+if [ $install_helm -eq 1 ]
+then
+     echo "INSTALL HELM"
+     curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+     chmod +x get_helm.sh
+     ./get_helm.sh
+fi
+
+echo "CONNECTING TO REGISTRY: $registry"
+export HELM_EXPERIMENTAL_OCI=1
+helm registry login -u $vmwareuser -p $vmwarepassword registry.tanzu.vmware.com
+helm pull "oci://$registry/tanzu-gemfire-for-kubernetes/gemfire-crd" --version $operator_version --destination ./
+helm pull "oci://$registry/tanzu-gemfire-for-kubernetes/gemfire-operator" --version $operator_version --destination ./
+
+echo "CREATE $clustername CLUSTER"
+helm install gemfire-crd "gemfire-crd-$operator_version.tgz" --namespace $namespace --set operatorReleaseName=gemfire-operator
+helm install gemfire-operator "gemfire-operator-$operator_version.tgz" --namespace $namespace
+helm ls --namespace $namespace
 
 echo "CREATE $clustername CLUSTER"
 ytt -f gemfire-crd.yml \
      --data-value-yaml cluster_name=$cluster_name \
-     --data-value-yaml image="imageregistry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-k8s:$version" \
+     --data-value-yaml image="imageregistry.pivotal.io/tanzu-gemfire-for-kubernetes/gemfire-k8s:$gemfire_version" \
      | $kubectl --namespace=$namespace apply -f-
 
 
